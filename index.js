4f4628a300dbc60550b396fefc01e831d86e2281
David Yoon <david.hyeshik@yahoo.com>
2024-07-24 08:22:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 // index.js
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+// "Hello, World!"를 콘솔에 출력하는 함수
+function printHelloWorld() {
+    console.log("Hello, World!");
+}
 
-// 기본 페이지에 "Hello, World!" 표시
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-// 서버 시작
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// 함수 호출
+printHelloWorld();
